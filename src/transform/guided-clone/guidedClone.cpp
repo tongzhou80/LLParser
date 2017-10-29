9ac829d97bc85f6c01d9036459cc6693eef65040
Tong <zt9465@gmail.com>
2017-10-29 17:48:25 -0400

    to add fortran/c++ support

--- src/transform/guided-clone/guidedClone.cpp
+++ src/transform/guided-clone/guidedClone.cpp
@@ -7,23 +7,44 @@
 #include <passes/pass.h>
 #include <asmParser/sysDict.h>
 #include <di/diSubprogram.h>
+#include <utilities/strings.h>
 
 class GuidedClonePass: public Pass {
     std::ofstream _ofs;
-    string _dir;
+    string _log_dir;
+    string _src_dir;
 public:
     GuidedClonePass() {
         set_is_global_pass();
-        _dir = ".";
+        _log_dir = ".";
+        _src_dir = ".";
+    }
+
+    Module* get_module(string& src_filename) {
+        
     }
 
     bool run_on_global() override {
         /* set source file dir */
-        if (has_argument("dir")) {
-            _dir = get_argument("dir");
+        if (has_argument("log_dir")) {
+            _log_dir = get_argument("log_dir");
+        }
+        if (has_argument("src_dir")) {
+            _src_dir = get_argument("src_dir");
         }
 
 
+        std::ifstream ifs(_log_dir+"/clone.log");
+        string line;
+        while (std::getline(ifs, line)) {
+            auto fields = Strings::split(line, ' ');
+            string callee = fields[0];  // name of the function that's to be cloned
+            string user = fields[2];
+            string callee_file = Strings::split(callee, ':').at(0);
+
+            Module* callee_m = get_module(callee_file);
+
+        }
 //        _ofs.open("function_to_file.txt");
 //        for (MetaData* md: module->unnamed_metadata_list()) {
 //            if (DISubprogram* sp = dynamic_cast<DISubprogram*>(md)) {
