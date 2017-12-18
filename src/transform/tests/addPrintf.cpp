0293d0ea623cd48dc97e5ff92323fec397e80c00
Tong <zt9465@gmail.com>
2017-12-18 15:35:33 -0500

    to do wrf pass on knl

--- src/transform/tests/addPrintf.cpp
+++ src/transform/tests/addPrintf.cpp
@@ -13,7 +13,7 @@ public:
 
     bool run_on_module(Module* module) override {
         GlobalVariable* str = IRBuilder::add_global_string(module, "test");
-        CallInst* ci = IRBuilder::create_printf_callinst(module, str);
+        CallInst* ci = IRBuilder::create_printf_callinst(module, str, "i8* %a");
         Function* f = module->get_function("main");
         auto bb = f->basic_block_list()[0];
         bb->insert_instruction(5, (Instruction*)ci);
