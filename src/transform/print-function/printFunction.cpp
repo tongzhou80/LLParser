2dc8942b01c2a4ba1c1b8a8896422d2b9645c6bc
Tong <zt9465@gmail.com>
2017-12-21 19:08:19 -0500

    to make run_on_xxx return void

--- src/transform/print-function/printFunction.cpp
+++ src/transform/print-function/printFunction.cpp
@@ -5,17 +5,22 @@
 #include <ir/irEssential.h>
 
 class PrintFunctionPass: public Pass {
+    size_t _inst_cnt;
 public:
     PrintFunctionPass() {
         set_is_function_pass();
+
+        _inst_cnt = 0;
     }
 
     bool run_on_function(Function* function) override {
         std::cout << function->name() << ": " << function->instruction_count() << " instructions" << std::endl;
+        _inst_cnt += function->instruction_count();
     }
 
     bool do_finalization(Module* M) override {
-        std::cout << "in total: " << M->function_list().size() << " functions" << std::endl;
+        std::cout << "in total: " << M->function_list().size() << " functions, "
+                  << _inst_cnt << " instructions" << std::endl;
     }
 };
 
